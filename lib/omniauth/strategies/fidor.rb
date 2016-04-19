e163a5c8a3998bc309d572c0b116d99497035164
Sasha Gerrand <rubygems-omniauth-fidor@sgerrand.com>
2016-04-19 12:44:11 +0100

    Remove unused use of auth_token_params (YAGNI)

--- lib/omniauth/strategies/fidor.rb
+++ lib/omniauth/strategies/fidor.rb
@@ -37,7 +37,7 @@ module OmniAuth
 
       def build_access_token
         verifier = request.params["code"]
-        client.auth_code.get_token(verifier, {:client_id => client.id, :redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
+        client.auth_code.get_token(verifier, {:client_id => client.id, :redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true)))
       end
 
       def client
